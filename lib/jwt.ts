d459fb260d99f7fea896bf1b949fecae00342bb5
DreamHigh0820 <dreamhigh0820@gmail.com>
2025-07-12 09:35:50 -0400

    add jwt_secret to the jwt.ts

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -5,13 +5,14 @@ interface JWTPayload {
   [key: string]: unknown;
 }
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET || "dc79394ec25ed97a6292caeecb64051823ab19a2180d3e0aae723babb58b7c2791754565a2c43b8a8e9e55b4cc67b2a7734f57721e10314df9d9549743515313"
 
 // The secret key must be converted to a Uint8Array for jose
 const getSecretKey = () => {
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined in environment variables.")
   }
+  console.log("JWT_SECRET", JWT_SECRET)
   return new TextEncoder().encode(JWT_SECRET)
 }
 
